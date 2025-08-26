91a29b5a0631c74b55501e8e8de6e676f4092a08
nfridjat <nfridjat@gmail.com>
2025-08-26 13:40:51 -0400

    Create PDFViewer.jsx

---
+++ src/components/PDFViewer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PDFViewer = () => {
+  return (
+    <div>
+      <h2>Visualiseur de PDF</h2>
+      {/* Intégration d'un PDF ici */}
+    </div>
+  );
+};
+
+export default PDFViewer;
